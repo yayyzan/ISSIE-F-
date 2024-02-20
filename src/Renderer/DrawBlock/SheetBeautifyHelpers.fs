c1372eb0c7df160ee34660210fb12410d58a94de
luigirinaldi <luigirinaldi@users.noreply.github.com>
2024-02-20 20:06:17 +0000

    feat: finished build funcs, 6 is already defined elsewhere

--- src/Renderer/DrawBlock/SheetBeautifyHelpers.fs
+++ src/Renderer/DrawBlock/SheetBeautifyHelpers.fs
@@ -11,27 +11,27 @@ open Operators
 // Other helpers identified by Team
 
 // Function 1 : The dimensions of a custom component symbol
-let getCustomComponenttDimensionB1R (customComponent : SymbolT.Symbol) : {|H:float;W:float|} =
+let getCustomComponentDimensionB1R (customComponent : SymbolT.Symbol) : {|H:float;W:float|} =
     let comp = Optic.get SymbolT.component_ customComponent
 
     {|H=comp.H;W=comp.W|}
 
-let setCustomComponenttDimensionB1W (newDims : {|H:float;W:float|}) (customComponent : SymbolT.Symbol) : SymbolT.Symbol =
+let setCustomComponentDimensionB1W (newDims : {|H:float;W:float|}) (customComponent : SymbolT.Symbol) : SymbolT.Symbol =
     let comp = Optic.get SymbolT.component_ customComponent
 
     customComponent 
     |> Optic.set SymbolT.component_ {comp with H = newDims.H; W = newDims.W}
 
-let componentDimension_ = Lens.create getCustomComponenttDimensionB1R setCustomComponenttDimensionB1W
+let componentDimension_B1RW = Lens.create getCustomComponentDimensionB1R setCustomComponentDimensionB1W
 
 
 // Function 2 : The position of a symbol on a sheet
-let setSymbolPositionB2W (symID : ComponentId) (sheet : SheetT.Model) (newPosition : XYPos) = 
-    let positionLens = (SheetT.symbolOf_ symID) >-> SymbolT.posOfSym_
+let setSymbolPositionB2W (symId : ComponentId) (sheet : SheetT.Model) (newPosition : XYPos) = 
+    let positionLens = (SheetT.symbolOf_ symId) >-> SymbolT.posOfSym_
     Optic.set positionLens newPosition sheet
 
 // Function 3 : Read/write the order of ports on a specified side of a symbol
-let orderOfPortsBySide_B3W (side : Edge) =  
+let orderOfPortsBySide_B3RW (side : Edge) =  
     let orderOfEdge_ = 
         ((fun (orderMap : Map<Edge, string list>) -> Map.find side orderMap),
         (fun (newOrder : string list) orderMap -> 
@@ -43,6 +43,28 @@ let orderOfPortsBySide_B3W (side : Edge) =
         )))
         ||> Lens.create  
     
-    SymbolT.portMaps_ 
-    >-> SymbolT.order_ 
+    SymbolT.portMaps_
+    >-> SymbolT.order_
     >-> orderOfEdge_
+
+// Function 4 : The reversed state of the inputs of a MUX2
+let reversedState_B4RW = Lens.create (fun a -> a.ReversedInputPorts) (fun s a -> {a with ReversedInputPorts = s})
+
+// Function 5 : The position of a port on the sheet. It cannot directly be written.
+let getPortPosInSheetB5R ( portId : string ) ( sheet : SheetT.Model ) = 
+    Symbol.getPortLocation None ( sheet ^. SheetT.symbol_ ) portId
+
+// Function 6 : The Bounding box of a symbol outline (position is contained in this)
+let getBoundingBoxOfSymbolOutlineB6R = Symbol.getSymbolBoundingBox
+
+let stransform_ = Lens.create (fun a -> a.STransform) (fun s a -> {a with STransform = s})
+let rotation_ = Lens.create (fun a -> a.Rotation) (fun s a -> {a with Rotation = s})
+let flip_ = Lens.create (fun a -> a.Flipped) (fun s a -> {a with Flipped = s})
+
+
+
+// Function 7 : The rotation state of a symbol
+let rotationOfSymbol_B7RW = stransform_ >-> rotation_
+
+// Function 8 : The flip state of a symbol
+let flipOfSymbol_B8RW = stransform_ >-> flip_
