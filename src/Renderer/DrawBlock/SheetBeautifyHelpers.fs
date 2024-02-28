ff3b3df32754fef22cabbd794fe255185ac15593
omaralkhatib03 <oa321@ic.ac.uk>
2024-02-28 18:23:51 +0000

    Tested T functions, Ensured that they work

--- src/Renderer/DrawBlock/SheetBeautifyHelpers.fs
+++ src/Renderer/DrawBlock/SheetBeautifyHelpers.fs
@@ -3,6 +3,7 @@
 //-----------------Module for beautify Helper functions--------------------------//
 // Typical candidates: all individual code library functions.
 // Other helpers identified by Team
+open System
 open CommonTypes
 open DrawModelType
 open DrawModelType.BusWireT
@@ -10,6 +11,7 @@ open DrawModelType.SymbolT
 open Optics
 open Optics.Operators
 open Sheet
+open SimulatorTypes
 open Symbol
 open BlockHelpers
 
@@ -91,11 +93,11 @@ let reverseStateMux2 (symbol: SymbolT.Symbol) : bool option =
 /// <param name="newReversedState"> The new reversed state of MUX2 symbol </param>
 /// <param name="symbol"> The MUX2 symbol being modified </param>
 /// <returns> The updated MUX2 </returns>
-let B4W (newReversedState: bool) (symbol: SymbolT.Symbol) : SymbolT.Symbol =
+let setMux2ReverseState (newReversedState: bool) (symbol: SymbolT.Symbol) : SymbolT.Symbol =
     {symbol with ReversedInputPorts = Some newReversedState}
 
 // NB: Prism instead of lens because state is bool option
-let reversedPrism = Prism.create reverseStateMux2 B4W
+let reversedPrism = Prism.create reverseStateMux2 setMux2ReverseState
 
 /// <summary> The Position of the port </summary>
 /// <param name="port"> The target post </param>
@@ -159,33 +161,19 @@ let flippedLens_ = Lens.create isSymbolFlipped setSymbolFlip
 // ------------------------    Test Functions    ------------------------------//
 // ----------------------------------------------------------------------------//
 
-/// <summary> Orders horizontal segments by the X position vertical segment by the Y position </summary>
-/// <remarks>Note this is not changed in the model, this is done to make it easier to analyze visible segments </remarks>
-/// <param name="aSegment"> ASegment to order </param>
-/// <returns> Modified A segment with Start and End ordered correctly </returns>
-let orderASegment (aSegment: ASegment) : ASegment =
-    let delta = aSegment.Start - aSegment.End
-    aSegment
-    |> (fun visSeg ->
-        match visSeg.Orientation, delta.X > 0, delta.Y < 0 with
-        | Horizontal, true, _ | Vertical, _, true  -> {aSegment with Start = visSeg.End; End = visSeg.Start}
-        | _ -> visSeg
-    )
 
-/// <summary> Returns the length of an ASegment </summary>
-/// <param name="aSegment"> ASegment </param>
-/// <returns> Length of ASegment as a float </returns>
-let getASegmentLength (aSegment: ASegment) : float =
+/// <summary> Gets the values of a map as a list </summary>
+/// <remarks> This is used quite often, it is much better to define it rather than use a lambda everytime </remarks>
+/// <param name="map"> Map of any type </param>
+/// <returns> The Values of the map as a list </returns>
+let mapValuesToList (map: Map<'a,'b> ) =
+    Seq.toList map.Values
+
+
+let getAbsSegmentLength (aSegment: ASegment) =
     squaredDistance aSegment.Start aSegment.End
 
 let (|IsEven|IsOdd|) (n: int) = match n % 2 with | 0 -> IsEven | _ -> IsOdd
-
-/// <summary> Returns the visible ASegments of a wire </summary>
-/// <returns> Non Zero ASegments of a wire with the correct order </returns>
-let visibleSegments (wire: Wire): ASegment list =
-    wire
-    |> getNonZeroAbsSegments
-    |> List.map orderASegment
 
 /// <summary> Return unique pairs of elements of a list </summary>
 /// <remarks> Unique in the sense that (a,b) = (b,a). Hence the function will only return 1 of these </remarks>
@@ -209,6 +197,32 @@ let improvedOverlap1D ((a1, a2): float * float) ((b1, b2): float * float) (overl
     | StrictOverlap -> a_max > b_min && b_max > a_min
     | WeakOverlap -> a_max >= b_min && b_max >= a_min
 
+let improvedOverlap2D ((a1, a2): XYPos * XYPos) ((b1, b2): XYPos * XYPos) (overlapType: OverlapType) : bool =
+    (improvedOverlap1D (a1.X, a2.X) (b1.X, b2.X) overlapType) && (improvedOverlap1D (a1.Y, a2.Y) (b1.Y, b2.Y) overlapType)
+
+let getWiresInBox' (box: BoundingBox) (model: BusWireT.Model) : (Wire * int) list =
+    let wires =
+        model
+        |> Optic.get wires_
+        |> mapValuesToList
+
+    let bottomRight =
+        {   X = box.TopLeft.X + box.W
+            Y = box.TopLeft.Y + box.H
+        }
+
+    // State Tuple - (overlapping: bool, overlapping_wire_index: int)
+    let checkOverlapFolder (startPos: XYPos) (endPos: XYPos) (state: bool * int) (segment: Segment) : bool * int =
+        let overlap = improvedOverlap2D (startPos, endPos) (box.TopLeft, bottomRight) StrictOverlap
+        (fst state || overlap), if overlap then segment.Index else snd state
+
+    List.map (fun w -> foldOverNonZeroSegs checkOverlapFolder (false, -1) w, w) wires
+    |> List.filter (fun l -> fst (fst l))
+    |> List.map (fun ((_, index), w) -> w, index)
+
+
+
+
 
 /// <summary> Checks if two visible ASegments overlap </summary>
 /// <param name="vertSeg"> The Vertical ASegment </param>
@@ -216,25 +230,87 @@ let improvedOverlap1D ((a1, a2): float * float) ((b1, b2): float * float) (overl
 /// <param name="overlapType"> The type of Comparison, (strict or weak) overlap </param>
 /// <returns> True if the visible Segments overlap </returns>
 let visibleSegmentOverlap (vertSeg: ASegment) (horSeg: ASegment) (overlapType: OverlapType) : bool =
-    let ({Start=vertStart; End=vertEnd} : ASegment) = vertSeg
+    let ({Start=vertStart; End=vertEnd} : ASegment) =  vertSeg
     let ({Start=horStart; End=horEnd} : ASegment) = horSeg
-
-    (improvedOverlap1D (vertStart.X, vertEnd.X) (horStart.X, horEnd.X) overlapType)
-    && (improvedOverlap1D (vertStart.Y, vertEnd.Y) (horStart.Y, horEnd.Y) overlapType)
+    improvedOverlap2D (vertStart, vertEnd) (horStart, horEnd) overlapType
 
 
-/// <summary> Gets the values of a map as a list </summary>
-/// <remarks> This is used quite often, it is much better to define it rather than use a lambda everytime </remarks>
-/// <param name="map"> Map of any type </param>
-/// <returns> The Values of the map as a list </returns>
-let mapValuesToList (map: Map<'a,'b> ) =
-    Seq.toList map.Values
+/// <summary> Orders horizontal segments by the X position vertical segment by the Y position </summary>
+/// <remarks>Note this is not changed in the model, this is done to make it easier to analyze visible segments </remarks>
+/// <param name="aSegment"> ASegment to order </param>
+/// <returns> Modified A segment with Start and End ordered correctly </returns>
+let orderASegment (aSegment: ASegment) : ASegment =
+    let delta = aSegment.Start - aSegment.End
+    aSegment
+    |> (fun visSeg ->
+        match visSeg.Orientation, delta.X > 0, delta.Y < 0 with
+        | Horizontal, true, _ | Vertical, _, true  -> {aSegment with Start = visSeg.End; End = visSeg.Start}
+        | _ -> visSeg
+    )
+
+let orderByOrientation (visSegments: ASegment list) (ori: Orientation) =
+       visSegments
+       |>   match ori with
+            | Horizontal -> List.groupBy (fun visSeg -> visSeg.Start.Y)
+            | Vertical -> List.groupBy (fun visSeg -> visSeg.Start.X)
+       |> List.map (fun (_, segments) ->
+           match ori with
+           | Horizontal -> List.sortBy (fun visSeg -> visSeg.Start.X) segments
+           | Vertical -> List.sortBy (fun visSeg -> visSeg.Start.Y) segments
+       )
+
+
+let removeOverlaps (aSegments: ASegment list) : ASegment list =
+    ([], aSegments)
+    ||> List.fold (fun visSegments visSeg ->
+        match visSegments with
+        | [] -> [visSeg]
+        | lst ->
+            let lastSegment = List.last lst
+            match visibleSegmentOverlap visSeg lastSegment WeakOverlap with
+            | false -> visSegments @ [visSeg]
+            | true -> visSegments[0..visSegments.Length-2] @ [{visSeg with End = visSeg.End}]
+    )
+
+(*
+The algorithm being used:
+1) Partition By Orientation
+2) Group Horizontal segments by Y, and sort on the start position
+3) Group Vertical segments by X, and sort by start position
+4)  Apply a removeOverlap fold, since the segments are grouped and sorted, we guarantee that any segments
+    That do overlap are consecutive. Thus rather than checking if a segment overlaps with all over segments,
+    we only check if the segment overlaps with the previous one.
+
+    Note that the Algorithm runs on ASegments after the order is corrected.
+*)
+let getVisibleSegments (aSegments: ASegment list) =
+    aSegments
+    |> List.partition (fun visSeg -> visSeg.Orientation = Horizontal)
+    |> (fun (hor, vert) -> orderByOrientation hor Horizontal, orderByOrientation vert Vertical)
+    |> (fun (hor, vert) -> List.collect removeOverlaps hor @ List.collect removeOverlaps vert)
+
+
+/// <summary> Returns the visible ASegments of a wire </summary>
+/// <returns> Non Zero ASegments of a wire with the correct order </returns>
+let getVisibleSegmentsOfWire (wire: Wire) =
+    wire
+    |> getNonZeroAbsSegments
+    |> List.map orderASegment
+    |> getVisibleSegments
+
+
+let getAllVisibleSegments (model: SheetT.Model) =
+    model
+    |> Optic.get SheetT.wires_
+    |> mapValuesToList
+    |> List.collect getAbsSegments
+    |> getVisibleSegments
 
 
 /// <summary> The number of pairs of symbols that intersect each other </summary>
 /// <param name="sheet"> The Sheet Model </param>
 /// <returns> The count over the whole sheet </returns>
-let intersectingSymbolPairs (sheet: SheetT.Model) : int =
+let intersectingSymbolPairs (sheet: SheetT.Model) : int = // T1R
     let bbs =
         sheet
         |> Optic.get SheetT.symbols_
@@ -247,23 +323,36 @@ let intersectingSymbolPairs (sheet: SheetT.Model) : int =
     |> Seq.length
 
 
+// Get Wires by nets, i.e Grouped by the OutputPort
+let getWireNets (model: BusWireT.Model) : Wire list list =
+    model
+    |> Optic.get wires_
+    |> mapValuesToList
+    |> List.groupBy (fun wire -> wire.OutputPort)
+    |> List.map snd
+
+
 /// <summary>  The number of distinct wire visible segments that intersect with one or more symbols </summary>
 /// <param name="model"> The BusWire model of the Sheet </param>
 /// <returns> The count over the whole sheet </returns>
-let countSymbolIntersectingSegments (model: BusWireT.Model) =
+let countSegmentsIntersectingSymbols (model: SheetT.Model) = // T2R
     model
-    |> Optic.get (symbol_ >-> symbols_)
+    |> Optic.get SheetT.symbols_
     |> mapValuesToList
     |> List.map getSymbolBoundingBox
-    |> List.collect (fun bb -> getWiresInBox bb model)
-    |> List.distinct
+    |> List.collect (fun bb -> getWiresInBox' bb model.Wire)
     |> List.length
 
 /// <summary> The number of distinct pairs of visible segments that cross each other at right angles </summary>
 /// <remarks>This does not include 0 segments, or segments on same net intersecting at one end, or segments on same net on top of each other. </remarks>
 /// <param name="model"> The Sheet model to count over </param>
 /// <returns> The count over the whole sheet </returns>
-let countRightAngleCrosses (model: SheetT.Model) : int =
+let countNumberOfJunctions (model: SheetT.Model)  = // T3R
+    let isASegmentsInSameNet (seg1: ASegment) (seg2: ASegment) =
+        let wires = Optic.get SheetT.wires_ model
+        let wire1 = Map.find (snd seg1.GetId) wires
+        let wire2 = Map.find (snd seg2.GetId) wires
+        wire1.OutputPort = wire2.OutputPort
 
     let visibleSegmentIntersect (seg1: ASegment) (seg2: ASegment) (overlapType: OverlapType): bool =
        if seg1.Orientation = seg2.Orientation
@@ -272,118 +361,54 @@ let countRightAngleCrosses (model: SheetT.Model) : int =
             | Vertical -> visibleSegmentOverlap seg1 seg2 overlapType
             | Horizontal -> visibleSegmentOverlap seg2 seg1 overlapType
 
-    let countWireIntersections (wire1: Wire, wire2: Wire) : int =
-        (visibleSegments wire1, visibleSegments wire2)
-        ||> List.map2 (fun seg1 seg2 ->
-            let OverlapType = match wire1.OutputPort = wire2.OutputPort with
-                                   | true -> StrictOverlap
-                                   | false -> WeakOverlap
-            visibleSegmentIntersect seg1 seg2 OverlapType
-        )
-        |> List.filter (fun bool -> bool = true)
-        |> List.length
-
-
     model
-    |> Optic.get SheetT.wires_
-    |> mapValuesToList
+    |> getAllVisibleSegments
     |> distinctPairs
-    |> (fun pairs ->
-        (0, pairs)
-        ||> List.fold (fun count pair -> count + countWireIntersections pair)
-    )
+    |> List.map (fun (seg1, seg2) -> if isASegmentsInSameNet seg1 seg2 then (seg1, seg2, StrictOverlap) else (seg1, seg2, WeakOverlap))
+    |> List.map (fun (seg1, seg2, overlap) -> visibleSegmentIntersect seg1 seg2 overlap)
+    |> List.filter (fun b -> b = true)
+    |> List.length
+
 
 
 /// <summary> Sum of wiring segment length, counting only one when there are N same-net segments overlapping </summary>
 /// <remarks>The number of distinct pairs of visible segments that cross each other at right angles </remarks>
-/// <param name="sheet"> The Sheet model to count over </param>
+/// <param name="model"> The Sheet model to count over </param>
 /// <returns> The count over the whole sheet </returns>
-(*
-The algorithm being used:
-1) Group Horizontal segments by Y, and sort on the start position
-2) Group Vertical segments by X, and sort by start position
-3)  Apply a removeOverlap fold, since the segments are grouped and sorted, we guarantee that any segments
-    That do overlap are consecutive. Thus rather than checking if a segment overlaps with all over segments,
-    we only check if the segment overlaps with the previous one.
 
-    Note that the Algorithm runs on ASegments after the order is corrected.
-*)
-let getVisibleWireLength (sheet: SheetT.Model) =
-    // Get Wires by nets, i.e Grouped by the OutputPort
-    let getWireNets (model: BusWireT.Model) : Wire list list =
-        model
-        |> Optic.get wires_
-        |> mapValuesToList
-        |> List.groupBy (fun wire -> wire.OutputPort)
-        |> List.map snd
+let getVisibleWireLength (model: SheetT.Model) = // T4R
+    let getASegmentLength (wireNet: Wire list) : float =
+        wireNet
+        |> List.collect getVisibleSegmentsOfWire
+        |> List.map (fun visSeg -> getAbsSegmentLength visSeg)
+        |> List.reduce (+)
 
-
-    let partitionWiresByOrientation (wires: Wire list) =
-        wires
-        |> List.collect visibleSegments
-        |> List.partition (fun visSeg -> visSeg.Orientation = Horizontal)
-
-
-
-    let orderByOrientation (visSegments: ASegment list) (ori: Orientation) =
-       visSegments
-       |>   match ori with
-            | Horizontal -> List.groupBy (fun pos -> pos.Start.Y)
-            | Vertical -> List.groupBy (fun pos -> pos.Start.X)
-       |> List.map (fun (_, segments) ->
-           match ori with
-           | Horizontal -> List.sortBy (fun pos -> pos.Start.X) segments
-           | Vertical -> List.sortBy (fun pos -> pos.Start.Y) segments
-       )
-
-    let removeOverlaps (visSegments:  ASegment list list) =
-        visSegments
-        |> List.collect (fun segments ->
-            ([], segments)
-            ||> List.fold (fun visSegments visSeg ->
-                match visSegments with
-                | [] -> [visSeg]
-                | lst ->
-                    let lastSegment = List.last lst
-                    match visibleSegmentOverlap visSeg lastSegment WeakOverlap with
-                    | false -> visSegments @ [visSeg]
-                    | true -> visSegments[0..visSegments.Length-2] @ [{lastSegment with End = visSeg.End}]
-            )
-        )
-
-    let getASegmentLength (visSegments: ASegment list) : float =
-       (0., visSegments)
-       ||> List.fold (fun currentLength seg -> currentLength + squaredDistance seg.Start seg.End)
-
-    sheet
+    model
     |> Optic.get SheetT.wire_
     |> getWireNets
-    |> List.map partitionWiresByOrientation
-    |> List.map (fun (hor,vert) -> orderByOrientation hor Horizontal, orderByOrientation vert Vertical)
-    |> List.collect (fun (hor, vert) -> removeOverlaps hor @ removeOverlaps vert)
-    |> getASegmentLength
+    |> List.map getASegmentLength
+    |> List.reduce (+)
+
 
 /// <summary> Number of visible wire right-angles </summary>
 /// <remarks>The number of distinct pairs of visible segments that cross each other at right angles </remarks>
 /// <param name="model"> The BusWire model of the Sheet </param>
 /// <returns> The count over the whole sheet </returns>
-let countVisibleRightAngles (model: BusWireT.Model) =
+let countRightAngles (model: SheetT.Model) = // T5R
     let countRightAngles (count: int) (segments: ASegment list) : int =
         segments
-        |> List.filter (fun seg -> not seg.IsZero)
         |> List.pairwise
         |> List.filter (fun (currSeg, nextSeg) -> currSeg.Orientation <> nextSeg.Orientation)
         |> (fun lst -> lst.Length + count)
 
     model
-    |> Optic.get wires_
+    |> Optic.get SheetT.wires_
     |> mapValuesToList
     |> List.map getNonZeroAbsSegments
     |> (fun segments ->
             (0, segments)
             ||> List.fold countRightAngles
         )
-
 
 
 // A sliding window for going through segments. It contains the portId if the window is the first of last window of a wire
@@ -406,7 +431,7 @@ let makeWindows (wire: Wire) (visSegments: ASegment list list) : Window list =
 /// <summary> The zero-length segments in a wire with non-zero segments on either side that have Lengths of opposite signs lead to a wire retracing itself </summary>
 /// <param name="sheet"> The Sheet model of the Sheet </param>
 /// <returns> The count of retracing segments, A list of all retracing segments, All retracing segments that intersect symbols </returns>
-let T6R (sheet: SheetT.Model) =
+let getRetracingSegments (sheet: SheetT.Model) =
     let segmentsIntersectsSymbol (segments: ASegment list) (portId: string)  : ASegment list =
         let sym =
             sheet
@@ -440,7 +465,7 @@ let T6R (sheet: SheetT.Model) =
         | a::b::c::_ -> b.IsZero && (sign a.Segment.Length <> sign c.Segment.Length)
         | _ -> false
 
-    let getRetracingSegments (wire: Wire) =
+    let getRetracingSegments' (wire: Wire) =
         let retracingSegmentsFold (foundSegments: ASegment list * ASegment list) (window: Window) =
             let allRetracingSegments, intersectingRetracingSegments = foundSegments
             match isSegmentsRetrace window with
@@ -462,4 +487,4 @@ let T6R (sheet: SheetT.Model) =
         ||> (fun retraceSeg intersectingRetraces -> retraceSeg.Length, retraceSeg, intersectingRetraces)
 
 
-    List.map getRetracingSegments wires
+    List.map getRetracingSegments' wires
