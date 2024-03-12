9bac59f93271aeabd3e3242f1ebd6bffa319f3bb
Jalexandrou <jalexandrou10@gmail.com>
2024-03-12 21:53:06 +0000

    Another example TestModel

--- src/Renderer/TestDrawBlockD3.fs
+++ src/Renderer/TestDrawBlockD3.fs
@@ -755,7 +755,7 @@ module D3Testing =
                 func testIndex 0 dispatch
         
         let testModelGen (model: Model) (dispatch: Dispatch<Msg>) =
-            // printfn $"{getTestModel model.Sheet}"
+            printfn $"{getTestModel model.Sheet}"
 
             let test2 : TestModel = 
                 { SimpleSymbols =
@@ -777,9 +777,37 @@ module D3Testing =
                       { Source = { Label = "G1"; PortNumber = 0 }
                         Target = { Label = "G2"; PortNumber = 1 } } ] }
 
-            // let test = getTestModel model.Sheet
+            let test3 = 
+                { SimpleSymbols =
+                    [ { SymLabel = "MUX1"
+                        CompType = Mux2
+                        Position = { X = 1595.5774438476562; Y = 1801.0249633789062 }
+                        STransform = { Rotation = Degree0; Flipped = false } }
+                      { SymLabel = "G2"
+                        CompType = GateN(And, 2)
+                        Position = { X = 1834.7874438476563; Y = 1886.0249633789062 }
+                        STransform = { Rotation = Degree0; Flipped = false } }
+                      { SymLabel = "ADD1"
+                        CompType = NbitsAdder 3
+                        Position = { X = 1844.4225561523438; Y = 1666.4750366210938 }
+                        STransform = { Rotation = Degree0; Flipped = false } }
+                      { SymLabel = "G1"
+                        CompType = GateN(And, 2)
+                        Position = { X = 1600.7874438476563; Y = 1692.5249633789062 }
+                        STransform = { Rotation = Degree0; Flipped = false } } ]
+                  Connections =
+                    [ { Source = { Label = "ADD1"; PortNumber = 1 }
+                        Target = { Label = "MUX1"; PortNumber = 2 } }
+                      { Source = { Label = "MUX1"; PortNumber = 0 }
+                        Target = { Label = "ADD1"; PortNumber = 1 } }
+                      { Source = { Label = "MUX1"; PortNumber = 0 }
+                        Target = { Label = "G2"; PortNumber = 0 } }
+                      { Source = { Label = "G1"; PortNumber = 0 }
+                        Target = { Label = "G2"; PortNumber = 1 } } ] }
 
-            let sheet = placeTestModel test2
+            let test = getTestModel model.Sheet
+
+            let sheet = placeTestModel test3
 
             showSheetInIssieSchematic sheet dispatch
 
